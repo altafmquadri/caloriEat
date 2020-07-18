1d67425b33a7669ca9e9af490853f93ca4745fc2
Altaf Quadri <altafmquadri@gmail.com>
2020-07-18 01:40:42 -0400

    moved out serving form into separate component

--- src/components/DisplayFood.js
+++ src/components/DisplayFood.js
@@ -1,32 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { fetchServing } from './actions/food'
+import ServingForm from './ServingForm'
+
 
 
 class DisplayFood extends Component {
-    state = {
-        quantity: 1,
-        measureURI: '',
-        foodId: ''
-    }
-
-    onSubmit = e => {
-        e.preventDefault()
-        if (!this.state.measureURI) return
-        this.setState({ foodId: this.props.food.foodId }, () => this.props.fetchServing({
-            quantity: parseInt(this.state.quantity),
-            measureURI: this.state.measureURI,
-            foodId: this.state.foodId
-        }))
-    }
-
-    onChangeServing = (e) => this.setState({ measureURI: e.target.value })
-    onChangeQuantity = (e) => this.setState({ quantity: e.target.value })
 
     render() {
         const displayContents = () => {
             if (this.props.food.foodItem) {
-                const { foodItem, calories, carbohydrates, fat, protein, fiber, image, measures } = this.props.food
+                const { foodItem, calories, carbohydrates, fat, protein, fiber, image } = this.props.food
                 const contents = (
                     <div>
                         <h1>{foodItem}</h1>
@@ -37,21 +20,7 @@ class DisplayFood extends Component {
                         <h3>Fiber: {fiber}g</h3>
                         <img alt={foodItem} src={image}></img>
                         <div>
-
-                            <form onSubmit={this.onSubmit}>
-                                <select
-                                    onChange={this.onChangeServing}
-                                    value={this.state.measureURI}>
-                                    {measures.map((measure, i) => <option key={i} value={measure.uri}>{measure.label}</option>)}
-                                </select>
-
-                                <input value={this.state.quantity}
-                                    type="text"
-                                    onChange={this.onChangeQuantity} />
-
-                                <button type="submit">Submit</button>
-
-                            </form>
+                            <ServingForm />
                         </div>
                     </div >
                 )
@@ -75,8 +44,6 @@ const mapStateToProps = (state) => ({
     food: state.food
 })
 
-const mapPropsToDispatch = (dispatch) => ({
-    fetchServing: obj => dispatch(fetchServing(obj))
-})
 
-export default connect(mapStateToProps, mapPropsToDispatch)(DisplayFood)
+
+export default connect(mapStateToProps)(DisplayFood)
---
+++ src/components/ServingForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { fetchServing } from './actions/food'
+
+class ServingForm extends Component {
+    state = {
+        quantity: 1,
+        measureURI: '',
+        foodId: ''
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        if (!this.state.measureURI) return
+        this.setState({ foodId: this.props.food.foodId }, () => this.props.fetchServing({
+            quantity: parseInt(this.state.quantity),
+            measureURI: this.state.measureURI,
+            foodId: this.state.foodId
+        }))
+    }
+
+    onChangeServing = (e) => this.setState({ measureURI: e.target.value })
+    onChangeQuantity = (e) => this.setState({ quantity: e.target.value })
+    render() {
+        const { measures } = this.props.food
+        return (
+
+            <form onSubmit={this.onSubmit}>
+                <select
+                    onChange={this.onChangeServing}
+                    value={this.state.measureURI}>
+                    {measures.map((measure, i) => <option key={i} value={measure.uri}>{measure.label}</option>)}
+                </select>
+
+                <input value={this.state.quantity}
+                    type="text"
+                    onChange={this.onChangeQuantity} />
+
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    food: state.food
+})
+
+const mapPropsToDispatch = (dispatch) => ({
+    fetchServing: obj => dispatch(fetchServing(obj))
+})
+
+export default connect(mapStateToProps, mapPropsToDispatch)(ServingForm)
