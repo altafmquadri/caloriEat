b63c1f0978bb3a4bcfb9e9cc7e281a16b975a7dd
Altaf Quadri <altafmquadri@gmail.com>
2020-07-18 02:14:21 -0400

    refactored DisplayFood component

--- src/components/DisplayFood.js
+++ src/components/DisplayFood.js
@@ -2,48 +2,44 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import ServingForm from './ServingForm'
 
-
-
 class DisplayFood extends Component {
 
     render() {
-        const displayContents = () => {
-            if (this.props.food.foodItem) {
-                const { foodItem, calories, carbohydrates, fat, protein, fiber, image } = this.props.food
-                const contents = (
-                    <div>
-                        <h1>{foodItem}</h1>
-                        <h3>Calories: {calories}</h3>
-                        <h3>Carbohydrates: {carbohydrates}g</h3>
-                        <h3>Fat: {fat}g</h3>
-                        <h3>Protein: {protein}g</h3>
-                        <h3>Fiber: {fiber}g</h3>
-                        <img alt={foodItem} src={image}></img>
-                        <div>
-                            <ServingForm />
-                        </div>
-                    </div >
-                )
-                return contents
-            } else return null
+        const {
+            calories,
+            carbohydrates,
+            fat,
+            foodItem,
+            fiber,
+            image,
+            protein
+        } = this.props.food
 
-        }
+        const contents = (
+            <div>
+                <h1>{foodItem}</h1>
+                <h3>Calories: {calories}</h3>
+                <h3>Carbohydrates: {carbohydrates}g</h3>
+                <h3>Fat: {fat}g</h3>
+                <h3>Fiber: {fiber}g</h3>
+                <h3>Protein: {protein}g</h3>
+                <img alt={foodItem} src={image}></img>
+                <div>
+                    <ServingForm />
+                </div>
+            </div >
+        )
+
         return (
             <div>
-                {displayContents()}
+                {this.props.food.foodItem ? contents : null}
             </div>
-
         )
     }
 }
 
-
-
-
 const mapStateToProps = (state) => ({
     food: state.food
 })
-
-
 
 export default connect(mapStateToProps)(DisplayFood)
