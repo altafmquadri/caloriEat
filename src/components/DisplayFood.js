705f73f557c67771c9844bb7db62cd078de3ccd7
Altaf Quadri <altafmquadri@gmail.com>
2020-07-18 01:22:43 -0400

    added form within display food to select serving and quantity

--- src/components/DisplayFood.js
+++ src/components/DisplayFood.js
@@ -5,21 +5,25 @@ import { fetchServing } from './actions/food'
 
 class DisplayFood extends Component {
     state = {
-        quantity: '',
+        quantity: 1,
         measureURI: '',
         foodId: ''
     }
 
-    onChange = e => {
-        this.setState({
-            quantity: 1,
-            measureURI: e.target.value,
-            foodId: this.props.food.foodId
-        }, () => this.props.fetchServing(this.state))
+    onSubmit = e => {
+        e.preventDefault()
+        if (!this.state.measureURI) return
+        this.setState({ foodId: this.props.food.foodId }, () => this.props.fetchServing({
+            quantity: parseInt(this.state.quantity),
+            measureURI: this.state.measureURI,
+            foodId: this.state.foodId
+        }))
     }
 
+    onChangeServing = (e) => this.setState({ measureURI: e.target.value })
+    onChangeQuantity = (e) => this.setState({ quantity: e.target.value })
+
     render() {
-        console.log(this.props);
         const displayContents = () => {
             if (this.props.food.foodItem) {
                 const { foodItem, calories, carbohydrates, fat, protein, fiber, image, measures } = this.props.food
@@ -32,18 +36,29 @@ class DisplayFood extends Component {
                         <h3>Protein: {protein}g</h3>
                         <h3>Fiber: {fiber}g</h3>
                         <img alt={foodItem} src={image}></img>
-                        <select
-                            onChange={this.onChange}
-                            value={this.state.measureURI}>
-                            {measures.map((measure, i) => <option key={i} value={measure.uri}>{measure.label}</option>)}
-                        </select>
+                        <div>
+
+                            <form onSubmit={this.onSubmit}>
+                                <select
+                                    onChange={this.onChangeServing}
+                                    value={this.state.measureURI}>
+                                    {measures.map((measure, i) => <option key={i} value={measure.uri}>{measure.label}</option>)}
+                                </select>
+
+                                <input value={this.state.quantity}
+                                    type="text"
+                                    onChange={this.onChangeQuantity} />
+
+                                <button type="submit">Submit</button>
+
+                            </form>
+                        </div>
                     </div >
                 )
                 return contents
             } else return null
 
         }
-        console.log(this.props);
         return (
             <div>
                 {displayContents()}
