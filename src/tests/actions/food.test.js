ba0ceef03e961c0fd56b885af6bab4daf078bd05
Altaf Quadri <altafmquadri@gmail.com>
2020-08-10 21:29:02 -0400

    add DotEnv file to SetupTests, add test for getFood, getServing, addFood, clearFood

---
+++ src/tests/actions/food.test.js
@@ -0,0 +1,33 @@
+import { getFood, getServing, addFood, clearFood } from '../../actions/food'
+import { food, measurement, category } from '../fixtures/food'
+
+describe('food actions', () => {
+    test('should get food item action object', () => {
+        const action = getFood(food)
+        expect(action).toEqual({
+            type: 'GET_FOOD',
+            payload: food
+        })
+    })
+    test('should get measurements to action object', () => {
+        const action = getServing(food, measurement)
+        expect(action).toEqual({
+            type: 'GET_SERVING',
+            payload: food,
+            obj: measurement
+        })
+    })
+    test('should add meal category to action object', () => {
+        const action = addFood(category)
+        expect(action).toEqual({
+            type: 'ADD_FOOD',
+            payload: category
+        })
+    })
+    test('should clear the food action object', () => {
+        const action = clearFood()
+        expect(action).toEqual({
+            type: 'CLEAR_FOOD'
+        })
+    })
+})
---
+++ src/tests/fixtures/food.js
@@ -0,0 +1,66 @@
+export const food = {
+    foodItem: 'milk',
+    foodId: 'food_b49rs1kaw0jktabzkg2vvanvvsis',
+    calories: 148,
+    carbohydrates: '11.71',
+    fat: '7.93',
+    fiber: '0.00',
+    protein: '7.69',
+    measures: [
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_serving',
+            label: 'Serving'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_gram',
+            label: 'Gram'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_ounce',
+            label: 'Ounce'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_pound',
+            label: 'Pound'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_kilogram',
+            label: 'Kilogram'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_tablespoon',
+            label: 'Tablespoon'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_cup',
+            label: 'Cup'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_bottle',
+            label: 'Bottle'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_fluid_ounce',
+            label: 'Fluid ounce'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_quart',
+            label: 'Quart'
+        },
+        {
+            uri: 'http://www.edamam.com/ontologies/edamam.owl#Measure_gallon',
+            label: 'Gallon'
+        }
+    ],
+    image: 'https://www.edamam.com/food-img/7c9/7c9962acf83654a8d98ea6a2ade93735.jpg',
+}
+
+export const measurement = {
+    quantity: 1,
+    measureURI: 'http://www.edamam.com/ontologies/edamam.owl#Measure_cup'
+}
+
+export const category = {
+    date: '2020-08-11T00:09:42.854Z',
+    mealCategory: 'breakfast'
+}
